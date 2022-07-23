1293284891bccb32379ade08c292086ec295d5cf
kalashjain23 <kalashjain513@gmail.com>
2022-07-23 16:26:54 +0530

    modified the text.py and created a dummy images folder for tests

--- server/text.py
+++ server/text.py
@@ -1,15 +1,34 @@
-from __future__ import print_function
 from google.cloud import vision
+import io
 
-image_uri = 'gs://cloud-vision-codelab/otter_crossing.jpg'
+path = 'server/images/image.png'
 
-client = vision.ImageAnnotatorClient()
-image = vision.Image()
-image.source.image_uri = image_uri
 
-response = client.text_detection(image=image)
-
-for text in response.text_annotations:
+def detect_text(path):
+    """Detects text in the file."""
     
-    print(text.description)
-    
+    client = vision.ImageAnnotatorClient()
+
+    with io.open(path, 'rb') as image_file:
+        content = image_file.read()
+
+    image = vision.Image(content=content)
+
+    response = client.text_detection(image=image)
+    texts = (response.text_annotations[0]).description
+    format_text = texts.replace('\n', " ")
+    print(format_text)
+
+    # for text in texts:
+    #     print('\n"{}"'.format(text.description))
+
+    #     vertices = (['({},{})'.format(vertex.x, vertex.y)
+    #                 for vertex in text.bounding_poly.vertices])
+
+    if response.error.message:
+        raise Exception(
+            '{}\nFor more info on error messages, check: '
+            'https://cloud.google.com/apis/design/errors'.format(
+                response.error.message))
+
+detect_text(path)
