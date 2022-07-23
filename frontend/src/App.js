42d37c44dd6ff3fd1b426c8dcfc380e2ec426967
AJITH P. KUMAR <ajithpkumar231@gmail.com>
2022-07-23 15:53:28 +0530

    App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+// import Basics from "./components/Basics"; //First file, contains basics
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import Navbar from "./components/Navbar";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Add from "./components/Add";
+import { createTheme, ThemeProvider } from "@mui/material";
+import { useState } from "react";
+// import Container from "@mui/material/Container";
 
 function App() {
+  const [mode, setMode] = useState("light");
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      {/* <Basics /> */}
+      {/* <Box sx={{ backgroundColor: "Red" }}> */}
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+          <Sidebar setMode={setMode} mode={mode} />
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
   );
 }
 
