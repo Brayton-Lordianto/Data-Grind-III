8b59b0e561ec0ae24ae9195e2ff73b9d595ad40c
chalory <chantalrain.pino@gmail.com>
2022-07-23 22:43:10 +0800

    first endpoint

--- server/app.py
+++ server/app.py
@@ -1,7 +1,21 @@
-from flask import Flask
-
+from email.mime import image
+from flask import Flask, jsonify, request
+from db import *
 app = Flask(__name__)
 
-@app.route("/")
-def hello_world():
-    
+
+
+@app.route('/api/v1/list', methods=['POST'])
+def add_entry():
+    print("p0")
+    request_json = request.get_json()
+    value1 = request_json.get('username')
+    value2 = request_json.get('text')
+    value3 = request_json.get('image_object')
+    if value1 is not None and value3 is not None:
+        data = insert_post(conn,username=value1, text=value2, image_object=value3)
+
+
+        
+        
+        return jsonify(data)
